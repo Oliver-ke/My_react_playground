461c13a9a2feffa46fb076a55605a9ec04ca09a0
Azorji Kelechi Oliver <32667315+Oliver-ke@users.noreply.github.com>
2019-02-20 18:15:17 +0100

    Initial commit

---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import styled from "react-emotion";
+
+// Create a <Title> react component that
+// renders an <h1> which is centered, plum and sized at 1.5em
+export default styled("h1")`
+  font-size: 1.5em;
+  text-align: center;
+  color: #2c2c2c;
+  margin: 0.5em;
+  padding: 0;
+`;
---
+++ src/Wrapper.js
@@ -0,0 +1,11 @@
+import styled from "react-emotion";
+
+// Create a <Wrapper> react component that renders a <section>
+// with some padding and a papayawhip background
+export default styled("section")`
+  padding: 2em;
+  background: white;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
